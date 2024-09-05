e5335888503b69b15d1d995b10f90e5aa068567a
ismail nyza <ismailnyzarsl@gmail.com>
2024-09-06 00:26:20 +0530

    added swagger initial set up

---
+++ src/main/java/parrotsl/akira/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package parrotsl.akira.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
---
+++ src/main/java/parrotsl/akira/service/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package parrotsl.akira.service;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Management API")
+                        .version("1.0")
+                        .description("This is the API documentation for the Task Management system.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
